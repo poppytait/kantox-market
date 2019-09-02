803b6d7ef6ad38ea78da3294f46ae46d033eccca
Poppy Tait <poppytait@Poppys-MacBook-Air.local>
2019-09-02 22:10:54 +0200

    cart style

--- app/discounts/StrawberryDiscount.js
+++ app/discounts/StrawberryDiscount.js
@@ -1,12 +1,14 @@
 import Discount from './Discount'
 
+const STRAWBERRY_ID = "2"
+
 class StrawberryDiscount extends Discount {
     isEligible(cart) {
-        return cart.some(item => item.product.id === "2" && item.quantity >= 3)
+        return cart.some(item => item.product.id === STRAWBERRY_ID && item.quantity >= 3)
     }
 
     calculateDiscount(cart) {
-        const { quantity, product: { price } } = cart.find(item => item.product.id === "2")
+        const { quantity, product: { price } } = cart.find(item => item.product.id === STRAWBERRY_ID)
 
         return (price - 4.5) * quantity
     }
